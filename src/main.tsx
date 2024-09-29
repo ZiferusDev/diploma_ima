9ca0db33088d0c7b9da46333b6d4ecf991e9aa05
Илья Барановский <baranovskiydev@gmail.com>
2024-09-29 10:02:31 +0300

    s

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { createHashRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Login } from './pages/Login';
 import { Register } from './pages/Register';
 import { Provider } from 'react-redux';
@@ -14,19 +14,24 @@ import './index.css';
 import { UsersTable } from './components/UsersTable';
 import { TasksTable } from './components/TasksTable';
 
-const router = createHashRouter([
+const router = createBrowserRouter(
+    [
+        {
+            path: '/',
+            element: <Main />,
+            children: [
+                { path: 'users', element: <UsersTable /> },
+                { path: 'tasks', element: <TasksTable /> },
+            ],
+        },
+        { path: '/login', element: <Login /> },
+        { path: '/register', element: <Register /> },
+        { path: '/*', element: <ErrorPage /> },
+    ],
     {
-        path: '/',
-        element: <Main />,
-        children: [
-            { path: 'users', element: <UsersTable /> },
-            { path: 'tasks', element: <TasksTable /> },
-        ],
+        basename: '/ima-front',
     },
-    { path: '/login', element: <Login /> },
-    { path: '/register', element: <Register /> },
-    { path: '/*', element: <ErrorPage /> },
-]);
+);
 
 createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
