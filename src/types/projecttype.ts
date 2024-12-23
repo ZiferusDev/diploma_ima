7f29a317796d3d0c656704d9b5319a9a6f36e1a3
Igor Krivtsov <ir-red5@mail.ru>
2024-12-23 16:51:13 +0300

    publish the app

--- src/types/projecttype.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "./usertype";
-
-export type Project = {
-    name: string;
-    id: string;
-    userProjects: User[] ;
-    gitlabLink: string | null,
-    githubLink: string | null
-}[]
